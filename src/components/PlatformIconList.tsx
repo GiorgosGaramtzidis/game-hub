c3d842bd0469c20edeb8c2d171ca1622fade9dee
GiorgosGaramtzidis <giorgosss@live.se>
2023-07-14 00:12:32 +0300

    Displaying Critic Score

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,14 +1,7 @@
-import {
-  FaWindows,
-  FaPlaystation,
-  FaXbox,
-  FaApple,
-  FaLinux,
-  FaAndroid,
-} from "react-icons/fa";
+import { FaWindows, FaXbox, FaApple, FaLinux, FaAndroid } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
+import { BsGlobe, BsPlaystation } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons/lib";
@@ -20,7 +13,7 @@ interface Props {
 const PlatformIconList = ({ platforms }: Props) => {
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
-    playstasion: FaPlaystation,
+    playstation: BsPlaystation,
     xbox: FaXbox,
     mac: FaApple,
     linux: FaLinux,
